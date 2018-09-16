0213290801a0030c1ee11a36967dacdfc93f045c
James <Jamesmiller0497@gmail.com>
2018-09-16 19:20:05 -0400

    First round

---
+++ BoardGame/GamePlay.py
@@ -0,0 +1,14 @@
+import random
+from BoardGame import Board
+
+
+def dice_roll(1, 6):
+
+
+class GamePlay:
+
+    board = Board.Board()
+    board.printBoard()
+
+    def movement(self):
+        move =
