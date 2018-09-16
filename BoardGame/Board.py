f98fc8e3c4bfc729bb30e376b3e474bddac93adb
JLRoper <JacobLRoper@gmail.com>
2018-09-16 19:16:53 -0400

    branch

--- BoardGame/Board.py
+++ BoardGame/Board.py
@@ -16,6 +16,8 @@ class Board:
         for row in self.board:
             print(row)
 
+    def setValue(self, token, row, column):
+        self.board[row][column] = token
 
 if __name__ == "__main__":
     myBoard = Board()
