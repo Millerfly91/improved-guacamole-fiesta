c560ce95341f1a8064cb77992f43cb6234782014
james <jamesmiller0497@gmail.com>
2018-09-29 17:51:57 -0400

    case changes for classes

--- BoardGame/Gameplay_two.py
+++ BoardGame/Gameplay_two.py
@@ -12,7 +12,7 @@ class GamePlay2:
     characterlocations = []
     charactertokens = []
 
-    def setupBoard(self):
+    def setupboard(self):
         boardbounds = input('What size board would you like to play?'
                             "\n'enter' for default board."
                             '\n(i.e. 4x5 , 10x10, 25x50) ')
@@ -25,12 +25,12 @@ class GamePlay2:
             width = int(bound[1])
 
         self.board = Board(length, width)
-        self.board.print_board()
+        # self.board.print_board()
 
-    def initializeCharacter(self, thisplayer):
+    def initializecharacter(self, thisplayer):
         player = baseCharacter()
         player.token = input("What token would {} like to be? ".format(thisplayer))
-        position = self.randomBoardPosition()
+        position = self.randomboardposition()
         player.charlocation = position
         player.charname = thisplayer
         player.health = 10
@@ -39,27 +39,27 @@ class GamePlay2:
         self.charactertokens.append(player.token)
         return player
 
-    def setupCharacters(self):
+    def setupcharacters(self):
         player = 1
         players = input('\nEnter the names of the people playing, '
                         'separated by a comma. ')
         playernames = players.split(',')
 
         for thisplayer in playernames:
-            newchar = self.initializeCharacter(thisplayer)
+            newchar = self.initializecharacter(thisplayer)
             newchar.player = player
             self.chardict.__setitem__('Player'+str(player), newchar)
             player += 1
             self.placecharacter(newchar)
             # print('chardict = ', list(self.chardict))
 
-        sel .printcharacterstats(self.chardict)
+        self.printcharacterstats()
 
-    def printcharactesStats(self, chardict):
-        charlist = chardict.values()
+    def printcharacterstats(self):
+        charlist = self.chardict.values()
 
         for name in charlist:
-            print(chardict.get(name))
+            print(self.chardict.get(name))
 
     def placecharacter(self, player):
         row = player.charlocation[0]
@@ -68,61 +68,61 @@ class GamePlay2:
         self.board.set_value(token, row, column)
         self.characterlocations.append(player.charlocation)
 
-    def randomBoardPosition(self):
+    def randomboardposition(self):
         row = random.randint(0, self.board.get_total_rows() - 1)
         column = random.randint(0, self.board.get_total_columns() - 1)
         return row, column
 
-    def placeTarget(self):
+    def placetarget(self):
         targettoken = '$'
-        position = self.randomBoardPosition()
+        position = self.randomboardposition()
         self.board.set_value(targettoken, position[0], position[1])
         self.targetlocation = position
 
-    def setupGame(self):
-        self.setupBoard()
-        self.setupCharacters()
-        self.placeTarget()
+    def setupgame(self):
+        self.setupboard()
+        self.setupcharacters()
+        self.placetarget()
         self.board.print_board()
 
-    def characterMovement(self, charname):
+    def charactermovement(self, charname):
         roll = random.randint(1, 6)
-        directional = input("{} has rolled a {}."
-                            "\n How would you like to use your turn?" 
+        directional = input("\n{} has rolled a {}."
+                            "\nHow would you like to use your turn?" 
                             "\n (ex. Up 2, Left 1) ".format(charname, roll))
-        self.validateMove(roll, directional)
+        self.validatemove(roll, directional)
         moves = directional.split(', ')
         for movement in moves:
             direction = movement.split(' ')
-            print('direction is: ', direction)
-            print('movement is: ', movement)
-        self.tokenMovement(moves, charname)
+            # print('direction is: ', direction)
+            # print('movement is: ', movement)
+        self.tokenmovement(moves, charname)
         # print('94 moves = ', moves)
         # print('95 charname = ', charname)
 
-    def tokenMovement(self, moves, charname):
+    def tokenmovement(self, moves, charname):
         # charstats = list(self.chardict)
         # chartoken = charstats[2]
         print('char tokens = ', self.charactertokens)
         # print(chartoken)
 
-    def validateMove(self, roll, movement):
+    def validatemove(self, roll, movement):
         steps = re.findall(r'\d+', movement)
         stepcount = 0
         for num in steps:
             stepcount += int(num)
-        print('steps are: ', steps)
-        print('stepcount is: ', stepcount)
+        # print('steps are: ', steps)
+        # print('stepcount is: ', stepcount)
         if stepcount != roll:
             raise ValueError('Movement is not equal to dice roll. Re evaluate your move.'
                              'Your roll was {}.'.format(roll))
 
-    def playGame(self):
+    def playgame(self):
         while not self.characterlocations.__contains__(self.targetlocation):
             for player in self.chardict:
-                self.characterMovement(player)
+                self.charactermovement(player)
 
 
 test = GamePlay2()
-test.setupGame()
-test.playGame()
+test.setupgame()
+test.playgame()
